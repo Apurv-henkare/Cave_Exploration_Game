fbd5cc8a050230ef357060781e8c4d0d4d113f8f
Apurv Henkare <apurvhenakare@gmail.com>
2023-07-15 16:09:39 +0530

    Main_Menu.lua Step 1 completed

---
+++ src/Dependencies.lua
@@ -0,0 +1,11 @@
+Class = require 'lib/class'
+push = require 'lib/push'
+Timer = require 'lib/knife.timer'
+Event = require 'lib/knife.event' 
+
+require 'src/constant'
+
+require 'src/states/StateStack'
+require 'src/states/BaseState'
+
+require 'src/states/game/MainMenu'
---
+++ src/states/game/MainMenu.lua
@@ -0,0 +1,23 @@
+--[[
+    GD50
+    Pokemon
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+]] MainMenu = Class {
+    __includes = BaseState
+}
+
+function MainMenu:init()
+
+end
+
+function MainMenu:update(dt)
+
+end
+
+function MainMenu:render()
+
+    love.graphics.print("main_menu", 100, 100)
+
+end
